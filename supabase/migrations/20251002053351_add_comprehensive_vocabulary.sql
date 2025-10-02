534cf8208a74aeadc77045fea065493db21ff5fd
DAFFA ZAIN <zaindaffa2005@gmail.com>
2025-10-03 00:38:17 +0700

    Update 20251002053351_add_comprehensive_vocabulary.sql

--- supabase/migrations/20251002053351_add_comprehensive_vocabulary.sql
+++ supabase/migrations/20251002053351_add_comprehensive_vocabulary.sql
@@ -16,13 +16,13 @@
   - Common Expressions (50 words total)
 */
 
--- People and Family - Adding remaining words to reach 50
+-- People and Family (contoh beberapa baris; lanjutkan pola yang sama)
 INSERT INTO vocabulary (topic_id, word, ipa, meaning_id, example_en, part_of_speech, tags, difficulty_level)
 SELECT 
   t.id,
   v.word,
   v.ipa,
-  v.meaning_id,
+  m.id AS meaning_id,
   v.example_en,
   v.part_of_speech,
   ARRAY[v.tags]::text[],
@@ -30,53 +30,55 @@ SELECT
 FROM (SELECT id FROM topics WHERE slug = 'people-and-family') t
 CROSS JOIN LATERAL (
   VALUES
-    ('nephew', '/ˈnefjuː/', 'nephew', 'My nephew is very smart.', 'noun', 'family', 2),
-    ('niece', '/niːs/', 'niece', 'I have a beautiful niece.', 'noun', 'family', 2),
-    ('husband', '/ˈhʌzbənd/', 'husband', 'Her husband is a doctor.', 'noun', 'family', 1),
-    ('wife', '/waɪf/', 'wife', 'His wife is a teacher.', 'noun', 'family', 1),
-    ('baby', '/ˈbeɪbi/', 'baby', 'The baby is sleeping.', 'noun', 'family', 1),
-    ('child', '/tʃaɪld/', 'child', 'Every child needs love.', 'noun', 'family', 1),
-    ('children', '/ˈtʃɪldrən/', 'children', 'The children are playing.', 'noun', 'family', 1),
-    ('teenager', '/ˈtiːneɪdʒə(r)/', 'teenager', 'She is a teenager now.', 'noun', 'social', 1),
-    ('adult', '/ˈædʌlt/', 'adult', 'He is an adult.', 'noun', 'social', 1),
-    ('classmate', '/ˈklɑːsmeɪt/', 'classmate', 'She is my classmate.', 'noun', 'social', 1),
-    ('best friend', '/best frend/', 'best friend', 'He is my best friend.', 'phrase', 'social', 1),
-    ('relative', '/ˈrelətɪv/', 'relative', 'We visit relatives often.', 'noun', 'family', 2),
-    ('twins', '/twɪnz/', 'twins', 'They are twins.', 'noun', 'family', 1),
-    ('stepfather', '/ˈstepfɑːðə(r)/', 'stepfather', 'My stepfather is kind.', 'noun', 'family', 2),
-    ('stepmother', '/ˈstepmʌðə(r)/', 'stepmother', 'Her stepmother loves her.', 'noun', 'family', 2),
-    ('stepsister', '/ˈstepsɪstə(r)/', 'stepsister', 'I have a stepsister.', 'noun', 'family', 2),
-    ('stepbrother', '/ˈstepbrʌðə(r)/', 'stepbrother', 'My stepbrother is nice.', 'noun', 'family', 2),
-    ('bride', '/braɪd/', 'bride', 'The bride looks beautiful.', 'noun', 'family', 2),
-    ('groom', '/ɡruːm/', 'groom', 'The groom is nervous.', 'noun', 'family', 2),
-    ('sibling', '/ˈsɪblɪŋ/', 'sibling', 'I have two siblings.', 'noun', 'family', 2),
-    ('orphan', '/ˈɔːfn/', 'orphan', 'The orphan needs help.', 'noun', 'family', 3),
-    ('widow', '/ˈwɪdəʊ/', 'widow', 'She is a widow.', 'noun', 'family', 3),
-    ('widower', '/ˈwɪdəʊə(r)/', 'widower', 'He is a widower.', 'noun', 'family', 3),
-    ('partner', '/ˈpɑːtnə(r)/', 'partner', 'They are business partners.', 'noun', 'social', 2),
-    ('couple', '/ˈkʌpl/', 'couple', 'The couple is happy.', 'noun', 'social', 1),
-    ('generation', '/ˌdʒenəˈreɪʃn/', 'generation', 'This is the new generation.', 'noun', 'family', 3),
-    ('ancestor', '/ˈænsestə(r)/', 'ancestor', 'Our ancestors were brave.', 'noun', 'family', 3),
-    ('descendant', '/dɪˈsendənt/', 'descendant', 'He is a descendant of kings.', 'noun', 'family', 3),
-    ('people', '/ˈpiːpl/', 'people', 'Many people came today.', 'noun', 'social', 1),
-    ('stranger', '/ˈstreɪndʒə(r)/', 'stranger', 'Don''t talk to strangers.', 'noun', 'social', 1),
-    ('guest', '/ɡest/', 'guest', 'We have guests tonight.', 'noun', 'social', 1),
-    ('leader', '/ˈliːdə(r)/', 'leader', 'She is a great leader.', 'noun', 'social', 2),
-    ('member', '/ˈmembə(r)/', 'member', 'I am a member of the club.', 'noun', 'social', 1),
-    ('community', '/kəˈmjuːnəti/', 'community', 'Our community is strong.', 'noun', 'social', 2),
-    ('in-laws', '/ɪn lɔːz/', 'in-laws', 'My in-laws are visiting.', 'noun', 'family', 3)
-) AS v(word, ipa, meaning_id, example_en, part_of_speech, tags, difficulty_level)
+    ('nephew', '/ˈnefjuː/',            'keponakan-laki-laki',   'My nephew is very smart.',            'noun',  'family', 2),
+    ('niece',  '/niːs/',               'keponakan-perempuan',   'I have a beautiful niece.',           'noun',  'family', 2),
+    ('husband','/ˈhʌzbənd/',           'suami',                 'Her husband is a doctor.',            'noun',  'family', 1),
+    ('wife',   '/waɪf/',               'istri',                 'His wife is a teacher.',              'noun',  'family', 1),
+    ('baby',   '/ˈbeɪbi/',             'bayi',                  'The baby is sleeping.',               'noun',  'family', 1),
+    ('child',  '/tʃaɪld/',             'anak',                  'Every child needs love.',             'noun',  'family', 1),
+    ('children','/ˈtʃɪldrən/',         'anak-anak',             'The children are playing.',           'noun',  'family', 1),
+    ('teenager','/ˈtiːneɪdʒə(r)/',     'remaja',                'She is a teenager now.',              'noun',  'social', 1),
+    ('adult',  '/ˈædʌlt/',             'dewasa',                'He is an adult.',                     'noun',  'social', 1),
+    ('classmate','/ˈklɑːsmeɪt/',       'teman-sekelas',         'She is my classmate.',                'noun',  'social', 1),
+    ('best friend','/best frend/',     'sahabat',               'He is my best friend.',               'noun',  'social', 1),
+    ('relative','/ˈrelətɪv/',          'kerabat',               'We visit relatives often.',           'noun',  'family', 2),
+    ('twins',  '/twɪnz/',              'kembar',                'They are twins.',                     'noun',  'family', 1),
+    ('stepfather','/ˈstepfɑːðə(r)/',   'ayah-tiri',             'My stepfather is kind.',              'noun',  'family', 2),
+    ('stepmother','/ˈstepmʌðə(r)/',    'ibu-tiri',              'Her stepmother loves her.',           'noun',  'family', 2),
+    ('stepsister','/ˈstepsɪstə(r)/',   'saudara-perempuan-tiri','I have a stepsister.',                'noun',  'family', 2),
+    ('stepbrother','/ˈstepbrʌðə(r)/',  'saudara-laki-laki-tiri','My stepbrother is nice.',             'noun',  'family', 2),
+    ('bride',  '/braɪd/',              'pengantin-perempuan',   'The bride looks beautiful.',          'noun',  'family', 2),
+    ('groom',  '/ɡruːm/',              'pengantin-laki-laki',   'The groom is nervous.',               'noun',  'family', 2),
+    ('sibling','/ˈsɪblɪŋ/',            'saudara-kandung',       'I have two siblings.',                'noun',  'family', 2),
+    ('orphan', '/ˈɔːfn/',              'yatim-piatu',           'The orphan needs help.',              'noun',  'family', 3),
+    ('widow',  '/ˈwɪdəʊ/',             'janda',                 'She is a widow.',                     'noun',  'family', 3),
+    ('widower','/ˈwɪdəʊə(r)/',         'duda',                  'He is a widower.',                    'noun',  'family', 3),
+    ('partner','/ˈpɑːtnə(r)/',         'pasangan',              'They are business partners.',         'noun',  'social', 2),
+    ('couple', '/ˈkʌpl/',              'pasangan-kekasih',      'The couple is happy.',                'noun',  'social', 1),
+    ('generation','/ˌdʒenəˈreɪʃn/',    'generasi',              'This is the new generation.',         'noun',  'family', 3),
+    ('ancestor','/ˈænsestə(r)/',       'leluhur',               'Our ancestors were brave.',           'noun',  'family', 3),
+    ('descendant','/dɪˈsendənt/',      'keturunan',             'He is a descendant of kings.',        'noun',  'family', 3),
+    ('people','/ˈpiːpl/',              'orang-orang',           'Many people came today.',             'noun',  'social', 1),
+    ('stranger','/ˈstreɪndʒə(r)/',     'orang-asing',           'Don''t talk to strangers.',           'noun',  'social', 1),
+    ('guest','/ɡest/',                 'tamu',                  'We have guests tonight.',             'noun',  'social', 1),
+    ('leader','/ˈliːdə(r)/',           'pemimpin',              'She is a great leader.',              'noun',  'social', 2),
+    ('member','/ˈmembə(r)/',           'anggota',               'I am a member of the club.',          'noun',  'social', 1),
+    ('community','/kəˈmjuːnəti/',      'komunitas',             'Our community is strong.',            'noun',  'social', 2),
+    ('in-laws','/ɪn lɔːz/',            'mertua-dan-besanan',    'My in-laws are visiting.',            'noun',  'family', 3)
+) AS v(word, ipa, meaning_slug, example_en, part_of_speech, tags, difficulty_level)
+JOIN meanings m
+  ON m.slug = v.meaning_slug AND m.language = 'id'
 WHERE NOT EXISTS (
   SELECT 1 FROM vocabulary WHERE word = v.word AND topic_id = t.id
 );
 
--- School and Classroom - Adding remaining words to reach 50
+-- School and Classroom (contoh beberapa baris; lanjutkan pola yang sama)
 INSERT INTO vocabulary (topic_id, word, ipa, meaning_id, example_en, part_of_speech, tags, difficulty_level)
 SELECT 
   t.id,
   v.word,
   v.ipa,
-  v.meaning_id,
+  m.id AS meaning_id,
   v.example_en,
   v.part_of_speech,
   ARRAY[v.tags]::text[],
@@ -84,43 +86,45 @@ SELECT
 FROM (SELECT id FROM topics WHERE slug = 'school-and-classroom') t
 CROSS JOIN LATERAL (
   VALUES
-    ('principal', '/ˈprɪnsəpl/', 'principal', 'The principal is strict.', 'noun', 'people', 2),
-    ('janitor', '/ˈdʒænɪtə(r)/', 'janitor', 'The janitor cleans the school.', 'noun', 'people', 2),
-    ('library', '/ˈlaɪbrəri/', 'library', 'I borrowed books from the library.', 'noun', 'place', 1),
-    ('laboratory', '/ləˈbɒrətri/', 'laboratory', 'We do experiments in the laboratory.', 'noun', 'place', 2),
-    ('canteen', '/kænˈtiːn/', 'canteen', 'We eat lunch in the canteen.', 'noun', 'place', 1),
-    ('notebook', '/ˈnəʊtbʊk/', 'notebook', 'Write in your notebook.', 'noun', 'object', 1),
-    ('dictionary', '/ˈdɪkʃənri/', 'dictionary', 'Use a dictionary to find words.', 'noun', 'object', 2),
-    ('sharpener', '/ˈʃɑːpnə(r)/', 'sharpener', 'I need a pencil sharpener.', 'noun', 'object', 1),
-    ('bag', '/bæɡ/', 'bag', 'My school bag is heavy.', 'noun', 'object', 1),
-    ('blackboard', '/ˈblækbɔːd/', 'blackboard', 'Write on the blackboard.', 'noun', 'object', 1),
-    ('whiteboard', '/ˈwaɪtbɔːd/', 'whiteboard', 'The teacher uses the whiteboard.', 'noun', 'object', 1),
-    ('chalk', '/tʃɔːk/', 'chalk', 'The teacher needs chalk.', 'noun', 'object', 1),
-    ('marker', '/ˈmɑːkə(r)/', 'marker', 'Use a marker on the whiteboard.', 'noun', 'object', 1),
-    ('map', '/mæp/', 'map', 'Look at the world map.', 'noun', 'object', 1),
-    ('schedule', '/ˈʃedjuːl/', 'schedule', 'Check the class schedule.', 'noun', 'document', 2),
-    ('break', '/breɪk/', 'break', 'We have a break at 10 AM.', 'noun', 'time', 1),
-    ('uniform', '/ˈjuːnɪfɔːm/', 'uniform', 'Wear your school uniform.', 'noun', 'object', 1),
-    ('subject', '/ˈsʌbdʒɪkt/', 'subject', 'Math is my favorite subject.', 'noun', 'education', 1),
-    ('grade', '/ɡreɪd/', 'grade', 'I got a good grade.', 'noun', 'education', 1),
-    ('report card', '/rɪˈpɔːt kɑːd/', 'report card', 'My report card is excellent.', 'phrase', 'document', 2),
-    ('projector', '/prəˈdʒektə(r)/', 'projector', 'The teacher uses a projector.', 'noun', 'equipment', 2),
-    ('computer', '/kəmˈpjuːtə(r)/', 'computer', 'We learn on computers.', 'noun', 'equipment', 1),
-    ('laptop', '/ˈlæptɒp/', 'laptop', 'I use a laptop for homework.', 'noun', 'equipment', 1),
-    ('stapler', '/ˈsteɪplə(r)/', 'stapler', 'I need a stapler.', 'noun', 'object', 2),
-    ('scissors', '/ˈsɪzəz/', 'scissors', 'Use scissors carefully.', 'noun', 'object', 1),
-    ('glue', '/ɡluː/', 'glue', 'We need glue for art class.', 'noun', 'object', 1),
-    ('crayon', '/ˈkreɪɒn/', 'crayon', 'Color with crayons.', 'noun', 'object', 1),
-    ('colored pencil', '/ˈkʌləd ˈpensl/', 'colored pencil', 'I have colored pencils.', 'phrase', 'object', 1),
-    ('painting', '/ˈpeɪntɪŋ/', 'painting', 'This is a beautiful painting.', 'noun', 'art', 2),
-    ('experiment', '/ɪkˈsperɪmənt/', 'experiment', 'We did a science experiment.', 'noun', 'activity', 2),
-    ('microscope', '/ˈmaɪkrəskəʊp/', 'microscope', 'Look through the microscope.', 'noun', 'equipment', 3),
-    ('schoolyard', '/ˈskuːljɑːd/', 'schoolyard', 'Play in the schoolyard.', 'noun', 'place', 1),
-    ('bell', '/bel/', 'bell', 'The school bell rang.', 'noun', 'object', 1),
-    ('assembly', '/əˈsembli/', 'assembly', 'We have assembly every Monday.', 'noun', 'activity', 2),
-    ('notice board', '/ˈnəʊtɪs bɔːd/', 'notice board', 'Read the notice board.', 'phrase', 'object', 2),
-    ('bookshelf', '/ˈbʊkʃelf/', 'bookshelf', 'The books are on the bookshelf.', 'noun', 'furniture', 1)
-) AS v(word, ipa, meaning_id, example_en, part_of_speech, tags, difficulty_level)
+    ('principal',   '/ˈprɪnsəpl/',           'kepala-sekolah',     'The principal is strict.',                        'noun', 'people',     2),
+    ('janitor',     '/ˈdʒænɪtə(r)/',         'penjaga-sekolah',    'The janitor cleans the school.',                  'noun', 'people',     2),
+    ('library',     '/ˈlaɪbrəri/',           'perpustakaan',       'I borrowed books from the library.',              'noun', 'place',      1),
+    ('laboratory',  '/ləˈbɒrətri/',          'laboratorium',       'We do experiments in the laboratory.',            'noun', 'place',      2),
+    ('canteen',     '/kænˈtiːn/',            'kantin',             'We eat lunch in the canteen.',                    'noun', 'place',      1),
+    ('notebook',    '/ˈnəʊtbʊk/',            'buku-catat',         'Write in your notebook.',                         'noun', 'object',     1),
+    ('dictionary',  '/ˈdɪkʃənri/',           'kamus',              'Use a dictionary to find words.',                 'noun', 'object',     2),
+    ('sharpener',   '/ˈʃɑːpnə(r)/',          'runcing-pensil',     'I need a pencil sharpener.',                      'noun', 'object',     1),
+    ('bag',         '/bæɡ/',                  'tas',                'My school bag is heavy.',                         'noun', 'object',     1),
+    ('blackboard',  '/ˈblækbɔːd/',           'papan-tulis-hitam',  'Write on the blackboard.',                        'noun', 'object',     1),
+    ('whiteboard',  '/ˈwaɪtbɔːd/',           'papan-tulis-putih',  'The teacher uses the whiteboard.',                'noun', 'object',     1),
+    ('chalk',       '/tʃɔːk/',               'kapur',              'The teacher needs chalk.',                        'noun', 'object',     1),
+    ('marker',      '/ˈmɑːkə(r)/',           'spidol',             'Use a marker on the whiteboard.',                 'noun', 'object',     1),
+    ('map',         '/mæp/',                 'peta',               'Look at the world map.',                          'noun', 'object',     1),
+    ('schedule',    '/ˈʃedjuːl/',            'jadwal',             'Check the class schedule.',                       'noun', 'document',   2),
+    ('break',       '/breɪk/',               'istirahat',          'We have a break at 10 AM.',                       'noun', 'time',       1),
+    ('uniform',     '/ˈjuːnɪfɔːm/',          'seragam',            'Wear your school uniform.',                       'noun', 'object',     1),
+    ('subject',     '/ˈsʌbdʒɪkt/',           'mata-pelajaran',     'Math is my favorite subject.',                    'noun', 'education',  1),
+    ('grade',       '/ɡreɪd/',               'nilai',              'I got a good grade.',                             'noun', 'education',  1),
+    ('report card', '/rɪˈpɔːt kɑːd/',        'rapor',              'My report card is excellent.',                    'noun', 'document',   2),
+    ('projector',   '/prəˈdʒektə(r)/',       'proyektor',          'The teacher uses a projector.',                   'noun', 'equipment',  2),
+    ('computer',    '/kəmˈpjuːtə(r)/',       'komputer',           'We learn on computers.',                          'noun', 'equipment',  1),
+    ('laptop',      '/ˈlæptɒp/',             'laptop',             'I use a laptop for homework.',                    'noun', 'equipment',  1),
+    ('stapler',     '/ˈsteɪplə(r)/',         'stapler',            'I need a stapler.',                               'noun', 'object',     2),
+    ('scissors',    '/ˈsɪzəz/',              'gunting',            'Use scissors carefully.',                         'noun', 'object',     1),
+    ('glue',        '/ɡluː/',                'lem',                'We need glue for art class.',                     'noun', 'object',     1),
+    ('crayon',      '/ˈkreɪɒn/',             'krayon',             'Color with crayons.',                             'noun', 'object',     1),
+    ('colored pencil','/ˈkʌləd ˈpensl/',    'pensil-warna',       'I have colored pencils.',                         'noun', 'object',     1),
+    ('painting',    '/ˈpeɪntɪŋ/',            'lukisan',            'This is a beautiful painting.',                   'noun', 'art',        2),
+    ('experiment',  '/ɪkˈsperɪmənt/',        'percobaan',          'We did a science experiment.',                    'noun', 'activity',   2),
+    ('microscope',  '/ˈmaɪkrəskəʊp/',        'mikroskop',          'Look through the microscope.',                    'noun', 'equipment',  3),
+    ('schoolyard',  '/ˈskuːljɑːd/',          'halaman-sekolah',    'Play in the schoolyard.',                         'noun', 'place',      1),
+    ('bell',        '/bel/',                 'bel',                'The school bell rang.',                           'noun', 'object',     1),
+    ('assembly',    '/əˈsembli/',            'apel/pengumpulan',   'We have assembly every Monday.',                  'noun', 'activity',   2),
+    ('notice board','/ˈnəʊtɪs bɔːd/',        'papan-pengumuman',   'Read the notice board.',                          'noun', 'object',     2),
+    ('bookshelf',   '/ˈbʊkʃelf/',            'rak-buku',           'The books are on the bookshelf.',                 'noun', 'furniture',  1)
+) AS v(word, ipa, meaning_slug, example_en, part_of_speech, tags, difficulty_level)
+JOIN meanings m
+  ON m.slug = v.meaning_slug AND m.language = 'id'
 WHERE NOT EXISTS (
   SELECT 1 FROM vocabulary WHERE word = v.word AND topic_id = t.id
 );
